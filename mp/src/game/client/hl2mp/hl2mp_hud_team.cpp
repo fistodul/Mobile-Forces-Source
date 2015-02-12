f937a689b3365881ed6495978538eb4847841a14
Filip <filipmymail@gmail.com>
2015-02-12 09:33:40 +0100

    Update hl2mp_hud_team.cpp

--- mp/src/game/client/hl2mp/hl2mp_hud_team.cpp
+++ mp/src/game/client/hl2mp/hl2mp_hud_team.cpp
@@ -138,6 +138,10 @@ void CTeamPlayHud::OnThink()
 	if ( pLocalPlayer == NULL )
 		 return;
 
+	// Ms - Don't render the team label if you are a spectator
+	if ( pLocalPlayer->GetTeamNumber() == TEAM_SPECTATOR )
+    		return;
+    
 	if ( HL2MPRules()->IsTeamplay() == false )
 		 return;
 
