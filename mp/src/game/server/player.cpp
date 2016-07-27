0a15da54e2785aa7b30eb330bfd10578c0808671
Filip <filipmymail@gmail.com>
2016-07-28 02:19:33 +0300

    Ayyy

--- mp/src/game/server/player.cpp
+++ mp/src/game/server/player.cpp
@@ -82,6 +82,8 @@
 #include "weapon_physcannon.h"
 #endif
 
+#include "vguiscreen.h"
+
 ConVar autoaim_max_dist( "autoaim_max_dist", "2160" ); // 2160 = 180 feet
 ConVar autoaim_max_deflect( "autoaim_max_deflect", "0.99" );
 
