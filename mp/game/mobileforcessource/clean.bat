e354189342096980ed0fa6fcc61af3f6b83d07aa
Filip <filipmymail@gmail.com>
2016-10-01 20:37:24 +0200

    Fixed nothing

--- mp/game/mobileforcessource/clean.bat
+++ mp/game/mobileforcessource/clean.bat
@@ -1,5 +1,5 @@
 @echo off
-rd "cache" "DownloadLists" "downloads" "lua_cache" "maps\graphs" "maps\soundcache" "materials\VGUI" "materialsrc" "SAVE" "screenshots" /s /q
+rd "cache" "DownloadLists" "downloads" "lua_cache" "maps\graphs" "maps\soundcache" "materialsrc" "SAVE" "screenshots" /s /q
 del "bin\client.pdb" "bin\Server.pdb" "cfg\config.cfg" "demoheader.tmp" "modelsounds.cache" "serverconfig.vdf" "stats.txt" "textwindow_temp.html" "voice_ban.dt" /f /s /q
 rd "..\..\..\dumps" /s /q
 del "..\..\*.mdmp" /f /s
